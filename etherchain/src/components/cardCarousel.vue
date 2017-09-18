2e18d6d04309d86108ebe9f8e461b5b3f7ec6066
liucong <liucong@metrodata.com>
2017-09-18 15:45:30 +0800

    add cards

--- etherchain/src/components/cardCarousel.vue
+++ etherchain/src/components/cardCarousel.vue
@@ -1,10 +1,13 @@
 <template>
 	<div class="mainCard">
-		<div class="cardCarousel">
-			<div v-for="token in cards">
-				<div>{{token.name}}</div>
-				<!-- {{token.data.img}} -->
-				<img :src="token.data.img" alt="omg">
+		<div class="cutCard">
+			<div class="cardCarousel">
+				<div v-for="token in cards" class="card">
+					<a href="#">
+						<img :src="token.data.img" alt="tokenimg">
+						<div class="tokenCardTitle">{{token.name}}</div>
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -25,7 +28,43 @@ export default{
 	background-color: #fff;
 }
 
+.cutCard{
+	padding: 20px 0;
+	overflow: hidden;
+}
+
 .cardCarousel{
-	border:red 1px solid;
+	/*border:red 1px solid;*/
+	width:1630px;
+	transition: .5s;
+}
+
+.cardCarousel:hover{
+	transform: translate(-202px,0);
+	transition: .5s;
+}
+
+.card{
+	background-color: #fff;
+	box-shadow: 0 1px 4px 2px rgba(0,34,207,.1);
+	border: none;
+	width: 150px;
+	height: 210px;
+	margin: 0 26px;
+	display: inline-block;
+	transition: .2s;
+}
+
+.card:hover{
+	box-shadow: 0 1px 16px 4px rgba(0,34,207,.1);
+}
+
+.card > a{
+	color: #4b5963;
+	text-decoration: none;
+}
+.tokenCardTitle{
+	padding: 12px 20px;
+	font-size: 18px;
 }
 </style>
