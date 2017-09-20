3d3ab65ebd0852245a0ddb287cd6bf4ee761f33a
Liu Cong <1434580150@qq.com>
2017-09-20 23:02:58 +0800

    done cards

--- etherchain/src/components/cardCarousel.vue
+++ etherchain/src/components/cardCarousel.vue
@@ -1,16 +1,26 @@
 <template>
 	<div class="mainCard">
-		<div class="cutCard">
 			<slot></slot>
-			<div class="cardCarousel">
-				<div v-for="token in cards" class="card">
-					<a href="#">
-						<img :src="token.data.img" alt="tokenimg">
-						<div class="tokenCardTitle">{{token.name}}</div>
-						<div>{{token.data.price.unit}}{{token.data.price.val}}<div class="change">{{token.data.price.change}}</div></div>
-					</a>
+			<div>
+				<i class="ion-ios-arrow-left carouselButton" @click="Arrow('left')"></i>
+				<div class="cutCard">
+					<div class="cardCarousel">
+						<div v-for="token in cards" class="card">
+							<a href="#">
+								<img :src="token.data.img" alt="tokenimg">
+								<div class="tokenCardTitle">{{token.name}}</div>
+								<div>{{token.data.price.unit}}{{token.data.price.val}}<div class="change">{{token.data.price.change}}</div></div>
+							</a>
+						</div>
+						<a href="#" class="card viewMore">
+							<div>
+								<p>查看更多</p>
+							</div>
+						</a>
+					</div>
 				</div>
-			</div>
+				<i class="ion-ios-arrow-right carouselButton" @click="Arrow('right')"></i>
+				</div>
 		</div>
 	</div>
 </template>
@@ -18,20 +28,52 @@
 <script>
 export default{
 	props:["cards"],
+	data:function(){
+		return {
+			cardCarousel:{
+				position:0,
+				step:0,
+				amount: 8,
+			},
+		}
+	},
 	methods:{
+		Arrow:function(direction){
+			let cardCarousel = document.getElementsByClassName("cardCarousel")[0];
+			let card = document.getElementsByClassName("card")[0],
+				width = card.offsetWidth,
+				marginLeft = window.getComputedStyle(card).marginLeft,
+				stepWidth = parseFloat(width) +parseFloat(marginLeft)*2;
+
+			if (direction === "left") {
+				if(this.cardCarousel.position > 0 && this.cardCarousel.position <= this.cardCarousel.amount-5){
+					this.cardCarousel.position -= 1;
+				}
+			} else {
+				if(this.cardCarousel.position >= 0 && this.cardCarousel.position < this.cardCarousel.amount-5){
+					this.cardCarousel.position += 1;
+				}
+			}
+
+			this.cardCarousel.step = stepWidth*this.cardCarousel.position;
+
+			cardCarousel.style.transform = "translate( -"+ this.cardCarousel.step +"px,0)";
+		},
 	},
 	watch:{
 	},
 	mounted:function(){
-		let elemChanged = document.getElementsByClassName('change');
-		for(let ele of elemChanged){
-			if(ele.innerHTML>0){
-				ele.style.color="green";
-				ele.innerHTML = "+" + ele.innerHTML;
-			}else{
-				ele.style.color="red";
+		(function(){
+			let elemChanged = document.getElementsByClassName('change');
+			for(let ele of elemChanged){
+				if(ele.innerHTML>0){
+					ele.style.color="green";
+					ele.innerHTML = "+" + ele.innerHTML;
+				}else{
+					ele.style.color="red";
+				}
 			}
-		}
+		})();
 	}
 }
 </script>
@@ -40,34 +82,51 @@ export default{
 .mainCard{
 	box-shadow: 0 1px 3px 0 rgba(0,34,77,.05);
 	border: 1px solid rgba(214,214,214,.6);
-	padding: 20px 45px 30px 45px;
+	padding: 40px 45px 30px 45px;
 	margin: 10px auto;
 	background-color: #fff;
 }
 
+.mainCard > div{
+	display: flex;
+	justify-content:space-between;
+	align-items:center;
+}
+
+.mainCard > div > i{
+	cursor: pointer;
+}
+
+.mainCard > div > i:hover {
+	color: #1038ff;
+}
+
 .cutCard{
-	padding: 20px 0;
+	/*border:red 1px solid;*/
+	padding-bottom: 20px;
 	overflow: hidden;
+	width: 96%;
 }
 
 .cardCarousel{
-	/*border:red 1px solid;*/
 	width:160%;
 	margin-top: 30px;
 	transition: .5s;
+	display: flex;
 }
 
-.cardCarousel:hover{
-	transform: translate(-202px,0);
-	transition: .5s;
+.carouselButton{
+	font-size: 30px;
+	display: inline-block;
 }
 
 .card{
+	border:red 1px solid;
 	background-color: #fff;
 	box-shadow: 0 1px 4px 2px rgba(0,34,207,.1);
 	border: none;
 	width: 9.8%;
-	height: 230px;
+	height: 220px;
 	margin: 0 1.3%;
 	display: inline-block;
 	transition: .2s;
@@ -81,6 +140,15 @@ export default{
 	color: #4b5963;
 	text-decoration: none;
 }
+
+.viewMore{
+	text-align:center;
+	padding-top: 80px;
+	text-decoration: none; 
+	color: #4c6bd8;
+	font-size: 18px;
+}
+
 .tokenCardTitle{
 	padding: 10px 20px;
 	font-size: 20px;
