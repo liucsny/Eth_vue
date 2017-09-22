8c06c78780493a4071c169025daf5523c52f886c
liucong <liucong@metrodata.com>
2017-09-22 11:44:36 +0800

    add shortblocklist

--- etherchain/src/components/blockList.vue
+++ etherchain/src/components/blockList.vue
@@ -14,21 +14,15 @@
 				<th>哈希率</th>
 				<th>区块奖励</th>
 			</tr>
-			<tr>
-				<td>a</td>
-				<td>s</td>
-				<td>d</td>
-				<td>f</td>
-				<td>g</td>
-				<td>h</td>
-			</tr>
-			<tr>
-				<td>12321w</td>
-				<td>2wsqasz</td>
-				<td>d</td>
-				<td>wqdqwd</td>
-				<td>dwz</td>
-				<td>dwd</td>
+			<tr v-for="block in shortBlockList">
+				<td><a href="#">{{block.blockHeight}}</a></td>
+				<td>{{block.time}}</td>
+				<td><a href="#">{{block.transactions}}</a></td>
+				<td><a href="#">{{compressString(block.miner,10)}}</a></td>
+				<td>{{block.gasLimit}}</td>
+				<td>{{block.difficulty}} TH</td>
+				<td>{{block.hashRate}} GH/s</td>
+				<td>{{block.blockReward}} Ether</td>
 			</tr>
 		</table>
 	</div>
@@ -37,7 +31,664 @@
 <script>
 
 export default {
-	components:{
+	data: function(){
+		return {
+			blockList:[
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+				{
+					blockHeight: 4254662,
+					time: "23秒前",
+					transactions: 216,
+					miner: "0x829BD824B016326A401d083B33D092293333A830",
+					gasLimit: 6712392,
+					difficulty: 2325.923,
+					hashRate: 92633.03,
+					blockReward: 5.41031,
+				},
+			],
+		}
+	},
+	methods:{
+		compressString:function(str,limit){
+			if(str.length>limit){
+				return str.substr(0,limit)+"...";
+			}
+				return str;
+		},
+	},
+	computed:{
+		shortBlockList:function(){
+			return this.blockList.splice(0, 20)
+		}
 	}
 }
 </script>
@@ -49,7 +700,7 @@ export default {
 #pageName{
 	font-size: 36px;
 	color: #67666e;
-	margin: 20px 0;
+	margin: 100px 0 30px 0;
 }
 
 table{
@@ -57,16 +708,34 @@ table{
 	width: 100%;
 }
 
+tr:nth-child(odd){
+	background-color: #F5F8FC;
+}
+
 tr{
-	background-color: #f0f3f7;
 	margin: 2px 0;
 	font-size: 14px;
 }
 
 tr > th{
 	/*background-color: #f0f3f7;*/
-	padding: 10px 0;
+	padding: 20px 0;
 	/*border:red 1px solid;*/
+	background-color: #F0F3F7;
 	margin: 0;
+}
+
+tr > td{
+	padding: 15px 0;
+	text-align: center;
+}
+
+tr > td > a{
+	color: #4c6bd8;
+	text-decoration: none;
+}
+tr > td > a:hover{
+	color: #1038ff;
+	text-decoration: underline;
 }
 </style>
